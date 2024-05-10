fff7c5b659367fb7a076a960e166d284e1dd4586
2275273 <68908577+2275273@users.noreply.github.com>
2024-05-10 10:40:00 +0800

    Create display-text.js

---
+++ display-text.js
@@ -0,0 +1,8 @@
+// display-text.js
+
+fetch('1.txt')
+    .then(response => response.text())
+    .then(text => {
+        document.getElementById('text-content').innerText = text;
+    })
+    .catch(error => console.error('Error fetching text:', error));

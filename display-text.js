6cd417eb0cac052d5fbbbbdec62c6b3b243c1ff9
2275273 <68908577+2275273@users.noreply.github.com>
2024-05-10 11:13:22 +0800

    Update display-text.js

--- display-text.js
+++ display-text.js
@@ -1,8 +1,8 @@
-// display-text.js
-
 fetch('1.txt')
     .then(response => response.text())
     .then(text => {
-        document.getElementById('text-content').innerText = text;
+        // 将换行符替换为 HTML 的换行标签
+        text = text.replace(/\n/g, "<br>");
+        document.getElementById('text-content').innerHTML = text;
     })
     .catch(error => console.error('Error fetching text:', error));
